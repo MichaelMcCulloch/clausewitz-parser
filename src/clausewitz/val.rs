f1e763be99f95563957e333815207833df68a359
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2022-09-09 20:46:36 -0600

    sda

--- src/clausewitz/val.rs
+++ src/clausewitz/val.rs
@@ -5,25 +5,7 @@ use std::{
 
 use chrono::NaiveDate;
 
-pub trait ClausewitzValue<'a> {
-    fn get_set_at_path<'b>(&'a self, path: &'b str) -> Result<&'a Vec<Val<'a>>, IndexError>;
-    fn get_date_at_path<'b>(&'a self, path: &'b str) -> Result<&'a NaiveDate, IndexError>;
-    fn get_string_at_path<'b>(&'a self, path: &'b str) -> Result<&'a str, IndexError>;
-    fn get_identifier_at_path<'b>(&'a self, path: &'b str) -> Result<&'a str, IndexError>;
-    fn get_decimal_at_path<'b>(&'a self, path: &'b str) -> Result<&'a f64, IndexError>;
-    fn get_integer_at_path<'b>(&'a self, path: &'b str) -> Result<&'a i64, IndexError>;
-    fn get_number_at_path<'b>(&'a self, path: &'b str) -> Result<f64, IndexError>;
-    fn get_array_at_path<'b>(&'a self, path: &'b str) -> Result<&'a Vec<Val<'a>>, IndexError>;
-    fn get_dict_at_path<'b>(
-        &'a self,
-        path: &'b str,
-    ) -> Result<&'a Vec<(&'a str, Val<'a>)>, IndexError>;
-    fn get_numbered_dict_at_path<'b>(
-        &'a self,
-        path: &'b str,
-    ) -> Result<(&'a i64, &'a Vec<(&'a str, Val<'a>)>), IndexError>;
-    fn get_at_path<'b>(&'a self, path: &'b str) -> Result<&'a Val<'a>, IndexError>;
-}
+use crate::ClausewitzValue;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Val<'a> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
 mod clausewitz;
 
+use chrono::NaiveDate;
 pub use clausewitz::{
     bracketed::key_value,
     root::{par_root, root},
-    val::{ClausewitzValue, IndexError, Val},
+    val::{IndexError, Val},
 };
+
+pub trait ClausewitzValue<'a> {
+    fn get_set_at_path<'b>(&'a self, path: &'b str) -> Result<&'a Vec<Val<'a>>, IndexError>;
+    fn get_date_at_path<'b>(&'a self, path: &'b str) -> Result<&'a NaiveDate, IndexError>;
+    fn get_string_at_path<'b>(&'a self, path: &'b str) -> Result<&'a str, IndexError>;
+    fn get_identifier_at_path<'b>(&'a self, path: &'b str) -> Result<&'a str, IndexError>;
+    fn get_decimal_at_path<'b>(&'a self, path: &'b str) -> Result<&'a f64, IndexError>;
+    fn get_integer_at_path<'b>(&'a self, path: &'b str) -> Result<&'a i64, IndexError>;
+    fn get_number_at_path<'b>(&'a self, path: &'b str) -> Result<f64, IndexError>;
+    fn get_array_at_path<'b>(&'a self, path: &'b str) -> Result<&'a Vec<Val<'a>>, IndexError>;
+    fn get_dict_at_path<'b>(
+        &'a self,
+        path: &'b str,
+    ) -> Result<&'a Vec<(&'a str, Val<'a>)>, IndexError>;
+    fn get_numbered_dict_at_path<'b>(
+        &'a self,
+        path: &'b str,
+    ) -> Result<(&'a i64, &'a Vec<(&'a str, Val<'a>)>), IndexError>;
+    fn get_at_path<'b>(&'a self, path: &'b str) -> Result<&'a Val<'a>, IndexError>;
+}
